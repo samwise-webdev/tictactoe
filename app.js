163ea213f4f8f4e347a510daddfd7ba05cc21da6
Sam <73436924+samwise-webdev@users.noreply.github.com>
2024-01-10 17:34:15 -0500

    removed Bootstrap for now, added imgs

---
+++ app.js
@@ -0,0 +1,9 @@
+const div = document.querySelector('div');
+
+div.addEventListener('click', () => {
+    img.querySelectorAll('img.x').hidden = false;
+})
+
+div.addEventListener('dblclick', () => {
+
+})
