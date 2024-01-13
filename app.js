eaaff7571cf116f3a607886aea3ca267bab2360a
Sam Burgett <samwise.burgett@outlook.com>
2024-01-13 17:09:26 -0500

    tried a bunch of different things that didn't work

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const div = document.querySelector('div');
+const div = document.querySelectorAll('div');
+const imagesX = document.querySelectorAll('.x');
+// div.addEventListener('click', () => {
+//     div.img.querySelectorAll('img').hidden = false;
+// }) doesn't work
+
+// document.querySelectorAll(img).forEach(img => img.addEventListener('click', () => img.hidden = false))  doesn't work
+
+// document.querySelectorAll(div).forEach(img => img.addEventListener('click', () => img.hidden = false)) doesn't work
+
+//     div.addEventListener('click', () => {
+//         imagesX.forEach(img => {
+//         img.hidden = false;
+//     })
+// }) turns all of them on :()
 
 div.addEventListener('click', () => {
-    img.querySelectorAll('img').hidden = false;
-})
-
-div.addEventListener('dblclick', () => {
-
+    x.hidden = false;
 })
