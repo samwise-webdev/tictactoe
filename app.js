211543d6acbcb62e85c67eb5018d76b95fa9d816
Sam Burgett <samwise.burgett@outlook.com>
2024-01-14 13:57:22 -0500

    just trying a bunch of different things w/ images

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const div = document.querySelectorAll('div');
-const imagesX = document.querySelectorAll('.x');
+// const div = document.querySelectorAll('div.img');
+
 // div.addEventListener('click', () => {
-//     div.img.querySelectorAll('img').hidden = false;
-// }) doesn't work
+//     hidden = false;
+// }) //doesn't work
 
 // document.querySelectorAll(img).forEach(img => img.addEventListener('click', () => img.hidden = false))  doesn't work
 
@@ -14,6 +14,34 @@ const imagesX = document.querySelectorAll('.x');
 //     })
 // }) turns all of them on :()
 
-div.addEventListener('click', () => {
-    x.hidden = false;
-})
+// div.addEventListener('click', () => {
+//     x.hidden = false;
+// })
+
+// img.addEventListener('click', () => {
+//         img.hidden = false;
+//     })
+
+// const img = document.querySelectorAll('div.img')
+// const imgsX = document.querySelectorAll('div img.x');
+// imgsX.forEach(img => addEventListener('click', toggleVis()));
+
+// const imgsO = document.querySelectorAll('div img.o');
+// imgsO.forEach(img => addEventListener('dblclick', toggleVis()));
+
+// function toggleVis() {
+//     if (img.hidden !== false) {
+//         img.hidden = false
+//     } else {
+//         img.hidden === true
+//     }
+// }  //was trying to see if i could toggle hidden using a separate function but that didn't work. don't know why i thought it might apply it to individual imgs instead?
+
+//-----------------------------------------------------------------
+
+const imgsX = document.querySelectorAll('div img.x');
+imgsX.forEach(img => addEventListener('click', () => img.hidden = false));
+
+const imgsO = document.querySelectorAll('div img.o');
+imgsO.forEach(img => addEventListener('dblclick', () => img.hidden = false));  //so this works to show all the images
+
